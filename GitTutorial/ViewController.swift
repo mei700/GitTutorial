702506945eb2e36c85430e74b5949864ec78e846
mei700 <35250838+mei700@users.noreply.github.com>
2018-09-24 13:09:04 +0700

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Jidapa Sae-Jiaw on 24/9/2561 BE.
+//  Copyright © 2561 Jidapa Sae-Jiaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
