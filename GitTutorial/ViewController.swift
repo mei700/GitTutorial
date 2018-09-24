90232e105317983594734e05c4b2b87c00fc1e93
mei700 <35250838+mei700@users.noreply.github.com>
2018-09-24 13:20:58 +0700

    Revert

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        print("hello CS193P")
-        print("hello world")
         super.viewDidLoad()
 
     }
